2fd7ead44d2679199fdbcbbb329d6db17f53321b
Julie Dursema <jmdursema@gmail.com>
2018-02-14 14:49:30 -0700

    set up actions, compnents, reducers, containers, and index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './Reducers';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+&& window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools, applyMiddleware(thunk));
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'))
 registerServiceWorker();
