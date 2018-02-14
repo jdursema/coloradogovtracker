e0c0c153ddc3af8f3220a47c5d88c171f8cf4c34
Julie Dursema <jmdursema@gmail.com>
2018-02-14 14:17:34 -0700

    add dependencies for redux

--- client/src/App.js
+++ client/src/App.js
@@ -9,29 +9,16 @@ class App extends Component {
     response: ''
   };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          Colorado Governer Tracker
         </header>
-        <p className="App-intro">{this.state.response}</p>
+        <div className="candidates">
+
+        </div>
       </div>
     );
   }
