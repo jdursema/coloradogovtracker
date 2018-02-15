9e7dafdee84cb3900e4a0b35a8e1ff03a19b6c6d
Julie Dursema <jmdursema@gmail.com>
2018-02-14 17:55:50 -0700

    display candidate in scrolling bar

--- client/src/Containers/CandidatesBar/CandidatesBar.js
+++ client/src/Containers/CandidatesBar/CandidatesBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { initialCandidatesFetch } from '../../Actions';
+import './CandidatesBar.css'
 
 export class CandidatesBar extends Component {
 
@@ -8,17 +9,28 @@ export class CandidatesBar extends Component {
     this.props.handleCandidates();
   }
 
+    
+
+  
+
   render() {
+    const mappedCardidates = this.props.candidates.map((candidate) => {
+      return <div className='candidate-img' style={{backgroundImage: `url(${candidate.image})`}}></div>
+    })
     return (
-      <div>
-        all the candidates
+      <div className='candidate-bar'>
+        {mappedCardidates}
       </div>
     )
   }
 }
 
+const mapStateToProps = state => ({
+  candidates: state.candidates
+})
+
 const mapDispatchToProps = dispatch => ({
   handleCandidates: () => dispatch(initialCandidatesFetch())
 })
 
-export default connect(null, mapDispatchToProps)(CandidatesBar);
+export default connect(mapStateToProps, mapDispatchToProps)(CandidatesBar);
