6f1414c02ca885b70ad6053b3ce8d2cb30975391
mariastlouis <msantray@yahoo.com>
2018-02-16 13:05:17 -0700

    selected candidate default state

--- client/src/Helper/helper.js
+++ client/src/Helper/helper.js
@@ -1,5 +1,5 @@
-// export const getCandidates = async () => {
-//   const candidateFetch = await fetch('/api/v1/candidates')
-//   const candidateResponse = await initialFetch.json()
-//   return candidateResponse
-// }
+export const getSelectedCandidate = async (id) => {
+  const candidateFetch = await fetch('/api/v1/candidates')
+  const candidateResponse = await initialFetch.json()
+  return candidateResponse
+}
