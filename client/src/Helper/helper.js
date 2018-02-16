de31d3ade575a94c60680c90d0e21d8d37c3ab1a
mariastlouis <msantray@yahoo.com>
2018-02-16 13:00:07 -0700

    reducer changed to allow selected candidate

---
+++ client/src/Helper/helper.js
@@ -0,0 +1,5 @@
+// export const getCandidates = async () => {
+//   const candidateFetch = await fetch('/api/v1/candidates')
+//   const candidateResponse = await initialFetch.json()
+//   return candidateResponse
+// }
