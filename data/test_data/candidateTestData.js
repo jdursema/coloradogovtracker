347e66bd43b722cdd500f7c7adfd6dbace4cec1b
Julie Dursema <jmdursema@gmail.com>
2018-02-17 16:46:35 -0700

    test zip query for specific candidates contributions

---
+++ data/test_data/candidateTestData.js
@@ -0,0 +1,24 @@
+module.exports = [  
+  {  
+    "committee_id":20165031883,
+    "candidate_id":20165031885,
+    "last_name":"Ginsburg",
+    "full_name":"Noel Ginsburg",
+    "committee_name":"Noel For Colorado",
+    "party":"Democrat",
+    "active":"TRUE",
+    "website":"https://www.noelforcolorado.com/",
+    "image":"http://msantray.fatcow.com/governor_photos/noel_ginsburg.jpeg"
+  },
+  {  
+    "committee_id":20175033116,
+    "candidate_id":20175033125,
+    "last_name":"Stapleton",
+    "full_name":"Walker Stapleton",
+    "committee_name":"Stapleton For Colorado",
+    "party":"Republican",
+    "active":"TRUE",
+    "website":"https://www.stapletonforcolorado.com/",
+    "image":"http://msantray.fatcow.com/governor_photos/walker_stapleton.png"
+  }
+]
