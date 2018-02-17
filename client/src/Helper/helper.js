fe81eafbcbe169e87ffbaac931397c31d35ea811
mariastlouis <msantray@yahoo.com>
2018-02-17 16:35:20 -0700

    cleaning selected candidate object

--- client/src/Helper/helper.js
+++ client/src/Helper/helper.js
@@ -8,6 +8,27 @@ export const initialCandidatesFetch = async () => {
 export const getSelectedCandidate = async (id) => {
   const candidateFetch = await fetch(`/api/v1/candidates/${id}`)
   const candidateResponse = await candidateFetch.json()
+  cleanCandidateInfo(candidateResponse.candidates)
   return candidateResponse.candidates
+  // return cleanCandidateInfo(candidateResponse.candidates)
 }
+
+const cleanCandidateInfo = (candidate) => {
+
+
+  // const contributionPromises = await getContributions(candidateObject.committee_id)
+  return {
+    id: candidate[0].committee_id,
+    info: {
+      name: candidate[0].full_name,
+      lastName: candidate[0].last_name,
+      image:candidate[0].image,
+      party: candidate[0].party,
+      website:candidate[0].website,
+      active:candidate[0].active,
+      committee: candidate[0].committee_name
+    }
+  }
+}
+
 
