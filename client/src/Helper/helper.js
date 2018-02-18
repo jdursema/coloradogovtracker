aae9ac1ca96484b9806e7e1b54ad846a995e5926
Julie Dursema <jmdursema@gmail.com>
2018-02-18 14:30:58 -0700

    create candidate totals object

--- client/src/Helper/helper.js
+++ client/src/Helper/helper.js
@@ -35,5 +35,35 @@ const cleanCandidateInfo = (candidate) => {
 export const getAllContributions = async () => {
   const initialFetch = await fetch ('/api/v1/contributions')
   const fetchResponse = await initialFetch.json();
-  return fetchResponse.contributors;
+  const cleanResponse = await cleanContribution(fetchResponse.contributors)
+  return cleanResponse
 }
+
+const cleanContribution = async (contribtionsArray) => {
+  const organizedContributions = contribtionsArray.reduce((acc, contribution) => {
+    if(!acc[contribution.committee_id]){
+      acc[contribution.committee_id] = []
+    }
+    
+    acc[contribution.committee_id].push(contribution)
+    return acc
+  }, {})
+  const candidateIds = Object.keys(organizedContributions)
+  console.log(88.01)
+  const object = candidateIds.map((candidate) => {
+    const candidateCash = organizedContributions[candidate].reduce((acc, contribution, index) => {
+      // acc += parseFloat(contribution.contribution_amount).toFixed(2)
+      acc += Math.floor(contribution.contribution_amount* 100) / 100
+      return acc
+    }, 0)
+  
+    return { committee_id: candidate,
+    candidate_name: organizedContributions[candidate][0].candidate_name,
+    candidate_contributions: organizedContributions[candidate].length,
+    candidate_cash: candidateCash}
+  })
+
+  return object
+
+}
+
