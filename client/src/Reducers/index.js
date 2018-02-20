514a383ea6c98f615bfc27d06e42ee1ebdc32017
Julie Dursema <jmdursema@gmail.com>
2018-02-20 12:24:52 -0700

    add expenditure reducer to root reducer

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import candidateReducer from './candidate-reducer';
 import selectedCandidateReducer from './selected-candidate-reducer';
 import contributionsReducer from './contribution-reducer.js';
+import expendituresReducer from './expenditure-reducer';
 
 
 const rootReducer = combineReducers({
   candidates: candidateReducer,
   contributions: contributionsReducer,
-  selctedCandidate: selectedCandidateReducer
+  selctedCandidate: selectedCandidateReducer,
+  expenditures: expendituresReducer
 });
 
 export default rootReducer;
