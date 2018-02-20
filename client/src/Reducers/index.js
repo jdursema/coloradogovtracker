67195a9e81d52c53462687169dc9d5b459dba87d
mariastlouis <msantray@yahoo.com>
2018-02-20 11:32:39 -0700

    state totals added to store and load with app

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import candidateReducer from './candidate-reducer';
 import selectedCandidateReducer from './selected-candidate-reducer';
 import contributionsReducer from './contribution-reducer.js';
+import stateTotalReducer from './state-total-reducer.js'
 
 
 const rootReducer = combineReducers({
   candidates: candidateReducer,
   contributions: contributionsReducer,
-  selectedCandidate: selectedCandidateReducer
+  selectedCandidate: selectedCandidateReducer,
+  stateTotals: stateTotalReducer
 });
 
 export default rootReducer;
