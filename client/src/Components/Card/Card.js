6787d5ed5a793b6ce29fb70dba4cbf5b6afd7ea7
mariastlouis <msantray@yahoo.com>
2018-02-21 23:21:16 -0700

    basic data rendering on contribution card

---
+++ client/src/Components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Card = ({key, id, firstName, lastName}) => {
+  return (
+    <div className = "contribution-card"> 
+     {firstName}
+    </div>
+  )
+}
+
+export default Card
--- client/src/Components/ContributionContainer/ContributionContainer.js
+++ client/src/Components/ContributionContainer/ContributionContainer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { withRouter} from 'react-router-dom';
 import * as actions from '../../Actions/';
 import { getSelectedCandidate } from '../../Helper/helper';
+import Card from '../Card/Card.js'
 
 
 class ContributionContainer extends Component {
@@ -15,28 +16,28 @@ class ContributionContainer extends Component {
   }
 
 
-
-// componentWillMount() {
-
-//   if(this.props.contributions.length) {
-//     this.setState({currentlyDisplayed: this.props.contributions})
-//   }
-// }
-
 componentWillReceiveProps(nextProps) {
   this.setState({currentlyDisplayed: this.props.contributions})
+  console.log(this.state.currentlyDisplayed)
 
 }
 
   mapContributions = (contributions, index) => {
-   if(contributions) {
-    const contributionMap = contributions.map((contribution, index) => {
-      return (
-        <div> {contribution.id}</div> 
-      )
-    })
-    return contributionMap
-   }
+    if(contributions) {
+      const contributionMap = contributions.map((contribution, index) => {
+        return (
+          <Card 
+            key = {index}
+            id = {contribution.id}
+            amount = {contribution.contribution_amount}
+            firstName = {contribution.donor_first}
+            lastName = {contribution.donor_last} />
+            
+            
+        )
+      })
+        return contributionMap;
+    }
   }
 
   render () {
