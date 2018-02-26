a7d0a8acdbc5f60e168471eb1405529806e9ba4a
mariastlouis <msantray@yahoo.com>
2018-02-26 10:15:44 -0700

    beginning card styles

--- client/src/Components/CandidateCard/CandidateCard.js
+++ client/src/Components/CandidateCard/CandidateCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const CandidateCard = (props) => {
   return (
-  <div>
-    <p>{props.info.name}</p>
+  <div className = "card">
+    <h3>{props.info.name}</h3>
     <p>{props.info.party}</p>
     <p>{props.info.contributionTotal.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
     <p>{props.info.expenditureTotal.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
